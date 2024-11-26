f23ee5649f9e034f247709a8c1bc84bead3566a9
ahtisham313 <130073639+ahtisham313@users.noreply.github.com>
2024-11-26 19:05:32 +0500

    used Routes.jsx instead of App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,18 @@
-import './App.css'
-// import LoginForm from './LoginForm'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './Home';
-import AdminLogin from './AdminLogin';
-function App() {
+// import './App.css'
+// // import LoginForm from './LoginForm'
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from './Home';
+// import AdminLogin from './AdminLogin';
+// function App() {
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<AdminLogin/>} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
-  )
-}
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<AdminLogin/>} />
+//         <Route path="/home" element={<Home />} />
+//       </Routes>
+//     </Router>
+//   )
+// }
 
-export default App
+// export default App
