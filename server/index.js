12bdc8c814234c5a7619f4f036b546766e4ae7f4
ahtisham313 <130073639+ahtisham313@users.noreply.github.com>
2024-11-26 18:59:10 +0500

    added some files to gitignore

--- server/index.js
+++ server/index.js
@@ -1,27 +1,4 @@
-// const express = require("express");
-// const cors = require("cors");
-// const bodyParser = require("body-parser");
-// const userR = require("./Routes/userRoutes"); // Correct file path
-// // Import user routes
 
-// const app = express();
-
-// // Middleware
-// // app.use(cors()); // Allow cross-origin requests
-// // const cors = require("cors");
-// app.use(cors({ origin: "http://localhost:5173" }));
-
-// app.use(bodyParser.json()); // Parse JSON bodies
-
-// // Routes
-// app.use("/userRoutes", userR); // Mount the user routes
-
-// // Server Configuration
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-// });
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
