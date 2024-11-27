f56a67e0b839bf06e365dbf2acdd5adef0bc0aaf
mubarakali826 <kingjohn135251@gmail.com>
2024-11-27 19:02:48 +0500

    folder structure correctiosn

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
+const authRoutes=require("./Routes/authRoutes")
+const employeeRoutes=require("./Routes/employeeRoutes")
+const adminRoutes=require("./Routes/adminRoutes")
 
 const app = express();
 
@@ -15,7 +18,9 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
-app.use("/auth", require("./Routes/authRoutes"));
+app.use("/auth",authRoutes);
+app.use("/employee",employeeRoutes);
+// app.use("/admin",adminRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
